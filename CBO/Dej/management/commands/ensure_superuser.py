8e4fc5d7d4a3260b5cb3610b9a84e418b3148ed8
Adrien Ehrhardt <UT2WLS@EMEA.CIB>
2021-12-10 18:05:15 +0100

    logger in ensure_superuser

--- CBO/Dej/management/commands/ensure_superuser.py
+++ CBO/Dej/management/commands/ensure_superuser.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-from loguru import logger
+from Dej.Print import MyPrinter
+
+logger = MyPrinter()
 
 SUPERUSER_ = 'Superuser '
 
